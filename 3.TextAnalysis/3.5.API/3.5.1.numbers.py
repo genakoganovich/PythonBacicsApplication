233c6654728b69f0164317aef52389bf6037770c
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-06-02 10:42:58 +0300

    0021: 3.5.1.numbers.py

---
+++ 3.TextAnalysis/3.5.API/3.5.1.numbers.py
@@ -0,0 +1,10 @@
+import requests
+
+answer = {True: 'Interesting', False: 'Boring'}
+
+with open('3.5.1.input.txt', 'r') as f:
+    numbers = map(lambda x: int(str(x)), f.readlines())
+
+for n in numbers:
+    res = requests.get(f'http://numbersapi.com/{n}/math?json=true')
+    print(answer[res.json()['found']])
